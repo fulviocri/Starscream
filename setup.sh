ae3874ca5f3c44f5568b64a904067269cd40dc52
Fulvio Cristallini <30806462+fulviocri@users.noreply.github.com>
2024-07-28 13:29:47 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,300 @@
+#!/bin/bash
+
+set +e
+#exec > >(while read line; do echo "$line"; do read "$line"; done) 2>&1
+
+if [ $(id -u) -ne 0 ]
+  then echo "Please run $0 as root"
+  exit 1
+fi
+
+clear
+
+echo -e "\e[31m"
+echo ""
+echo "  █████████   █████                         █████████                                                       ";
+echo " ███░░░░░███ ░░███                         ███░░░░░███                                                      ";
+echo "░███    ░░░  ███████    ██████   ████████ ░███    ░░░   ██████  ████████   ██████   ██████   █████████████  ";
+echo "░░█████████ ░░░███░    ░░░░░███ ░░███░░███░░█████████  ███░░███░░███░░███ ███░░███ ░░░░░███ ░░███░░███░░███ ";
+echo " ░░░░░░░░███  ░███      ███████  ░███ ░░░  ░░░░░░░░███░███ ░░░  ░███ ░░░ ░███████   ███████  ░███ ░███ ░███ ";
+echo " ███    ░███  ░███ ███ ███░░███  ░███      ███    ░███░███  ███ ░███     ░███░░░   ███░░███  ░███ ░███ ░███ ";
+echo "░░█████████   ░░█████ ░░████████ █████    ░░█████████ ░░██████  █████    ░░██████ ░░████████ █████░███ █████";
+echo " ░░░░░░░░░     ░░░░░   ░░░░░░░░ ░░░░░      ░░░░░░░░░   ░░░░░░  ░░░░░      ░░░░░░   ░░░░░░░░ ░░░░░ ░░░ ░░░░░ ";
+echo ""
+echo -e "\e[0m"
+
+# ========================================================================================================================================================================
+# Setting the password for the root user account
+set_root_password() {
+	echo ""
+	echo "Setting a password for root user account:"
+	read -p "New password: " -s root_password_1
+	echo ""
+	read -p "Retype new password: " -s root_password_2
+	echo ""
+
+	if [ $root_password_1 != $root_password_2 ]; then
+		echo "Passwords do not match"
+		setrootpassword
+	fi
+
+	#echo "root:$root_password_1" | chpasswd -e
+	echo -e "$root_password_1\n$root_password_1" | passwd root
+
+	if [ $? -eq 0 ]; then
+		echo "Password changed successfully"
+	else
+		echo "Password change error"
+		exit 1
+	fi
+
+	unset root_password_1
+	unset root_password_2
+	echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# Setting the current date & time
+change_current_datetime() {
+	echo ""
+	echo "Setting the current date and time:"
+	date
+	read -p "Is the current date and time correct? (y/n): " correct_date
+
+	if [ $correct_date == "n" ] || [ $correct_date == "N" ]; then
+		read -p "Enter date and time (YYYY-MM-DD HH:MM:SS): " current_date
+		timedatectl set-ntp false
+		timedatectl set-time "$current_date"
+
+		if [ $? -eq 0 ]; then
+			timedatectl set-ntp true
+			date
+		else
+			changecurrentdatetime
+		fi
+	fi
+
+	unset correct_date
+	unset current_date
+	echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# Setting FQDN host name
+set_hostname() {
+  echo ""
+  echo "Setting host name:"
+  read -p "Type the host name: " host_name
+  
+  hostnamectl set-hostname $host_name.cybertron.local
+  
+  unset host_name
+  echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# Cleaning up system
+cleanup_system() {
+  echo ""
+  read -p "Cleaning up system. [Press enter to continue]"
+  
+  if systemctl -all list-unit-files systemd-networkd-wait-online.service | grep "systemd-networkd-wait-online.service enabled" ;then
+    echo "Disabling systemd-networkd-wait-online.service"
+    systemctl disable systemd-networkd-wait-online.service
+    systemctl stop systemd-networkd-wait-online.service
+  fi
+  
+  if systemctl -all list-unit-files triggerhappy.service | grep "triggerhappy.service enabled" ;then
+    echo "Disabling triggerhappy.service"
+    systemctl disable triggerhappy.service
+    systemctl stop triggerhappy.service
+    
+    systemctl disable triggerhappy.socket
+    systemctl stop triggerhappy.socket
+  fi
+  
+  if systemctl -all list-unit-files ModemManager | grep "ModemManager enabled" ;then
+    echo "Uninstalling ModemManager and old GCC versions"
+    apt-get remove --purge -y modemmanager
+    apt-get remove --purge -y gcc-7-base gcc-8-base gcc-9-base
+  fi
+  
+  echo "Removing unused packages"
+  apt-get autoremove --purge -y
+  
+  echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# System update
+system_update() {
+  echo ""
+  read -p "Starting system update. [Press enter to continue]"
+  
+  UPDATENUM=$(apt-get -q -y --ignore-hold --allow-change-held-packages --allow-unauthenticated -s dist-upgrade | /bin/grep  ^Inst | wc -l)
+  
+  echo "Package to update: $UPDATENUM"
+  
+  if [[ $UPDATENUM > 0 ]]; then
+    apt-get update
+    apt-get -y upgrade
+  fi
+  
+  unset UPDATENUM
+  echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# Installing base component
+install_base_component() {
+  echo ""
+  read -p "Installing base component. [Press enter to continue]"
+  
+  apt-get install -y build-essential git curl xsltproc rsync tmux
+  
+  echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# Installing networking component
+install_network_component() {
+	echo ""
+	read -p "Installing networking component. [Press enter to continue]"
+
+  apt-get install -y i2c-tools ufw
+  sudo apt install -y python3-pip python3-venv python3-smbus
+  sudo apt install -y python3-netifaces python3-requests python3-nmap python3-scapy
+  sudo apt install -y nmap tcpdump doscan nast ettercap-text-only ncat
+  sudo apt install -y arping arpon arp-scan arpwatch
+  sudo apt install -y dhcpdump dhcp-probe dhcping dhcpig dns2tcp dhcpstarv
+  sudo apt install -y dnsenum dnsmap dnsrecon dnswalk dnsutils dnstracer
+  sudo apt install -y backdoor-factory masscan netdiscover macchanger
+  
+	echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# Configuring UFW firewall
+configure_network() {
+	echo ""
+	read -p "Configuring UFW firewall. [Press enter to continue]"
+
+	rfkill unblock wifi
+
+	for filename in /var/lib/systemd/rfkill/*:wlan ; do
+		echo 0 > $filename
+	done
+
+	ufw default deny incoming
+	ufw default allow outgoing
+	ufw deny in on eth0
+	ufw allow in on wlan0 to any port ssh
+	yes | ufw enable
+
+	echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# Settings Aliases for User K4l1m3r0
+set_user_aliases() {
+  cat >/home/k4l1m3r0/.bash_aliases <<EOL
+alias ..='cd ..'
+alias cls='clear'
+alias df='df -h'
+alias edit='nano'
+alias fastping='ping -c 100 -s.2'
+alias free='free -h'
+alias h='history'
+alias halt='sudo /sbin/halt'
+alias j='jobs -l'
+alias ll='ls -lah --color=auto'
+alias meminfo='free -m -l -t'
+alias mount='mount |column -t'
+alias nan='nano'
+alias now='date +"%T"'
+alias path='echo -e ${PATH//:/\\n}'
+alias ping5='ping -c 5'
+alias ports='netstat -tulanp'
+alias pscpu='ps auxf | sort -nr -k 3'
+alias psmem='ps auxf | sort -nr -k 4'
+alias reboot='sudo /sbin/reboot'
+alias root='sudo -i'
+alias shutdown='sudo /sbin/shutdown -h now'
+alias su='sudo -i'
+alias wanip='curl -w "\n" http://whatismyip.akamai.com/'
+alias wget='wget -c'
+  EOL
+}
+
+# ========================================================================================================================================================================
+# Settings Aliases for Root
+set_root_aliases() {
+  cat >/root/.bash_aliases <<EOL
+alias ..='cd ..'
+alias cls='clear'
+alias df='df -h'
+alias edit='nano'
+alias fastping='ping -c 100 -s.2'
+alias free='free -h'
+alias h='history'
+alias halt='sudo /sbin/halt'
+alias j='jobs -l'
+alias ll='ls -lah --color=auto'
+alias meminfo='free -m -l -t'
+alias mount='mount |column -t'
+alias nan='nano'
+alias now='date +"%T"'
+alias path='echo -e ${PATH//:/\\n}'
+alias ping5='ping -c 5'
+alias ports='netstat -tulanp'
+alias pscpu='ps auxf | sort -nr -k 3'
+alias psmem='ps auxf | sort -nr -k 4'
+alias reboot='sudo /sbin/reboot'
+alias root='sudo -i'
+alias shutdown='sudo /sbin/shutdown -h now'
+alias su='sudo -i'
+alias wanip='curl -w "\n" http://whatismyip.akamai.com/'
+alias wget='wget -c'
+  EOL
+}
+
+# ========================================================================================================================================================================
+# Setting the current date & time
+change_system_locale() {
+	echo ""
+	read -p "Configuring the System Locale. [Press enter to continue]"
+
+	rm -f /etc/localtime
+	echo "Europe/Rome" >/etc/timezone
+	dpkg-reconfigure -f noninteractive tzdata
+	dpkg-reconfigure -f noninteractive keyboard-configuration
+
+	echo "DONE"
+}
+
+# ========================================================================================================================================================================
+# Setup completed
+setup_complete() {
+	echo ""
+	read -p "StarScream Setup completed. [Press enter to reboot]"
+	reboot
+}
+
+set_user_aliases
+set_root_aliases
+
+
+
+set_root_password
+change_current_datetime
+#set_hostname
+cleanup_system
+system_update
+install_base_component
+install_network_component
+configure_network
+copy_config_files
+python_libs
+customize_telegram_bot
+change_system_locale
+setup_complete
